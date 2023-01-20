fa702bb3291417d64d296eadd5b3cb75436b8364
Paul <moicoxoxo@gmail.com>
2023-01-19 21:51:08 -0400

    update name

--- db/seeders/20220802155609-sucursales.js
+++ db/seeders/20220802155609-sucursales.js
@@ -5,14 +5,14 @@ module.exports = {
       [
         {
           id: 'ad8cd9f1-1028-4c5f-ae20-3ed58113013d',
-          nombre: 'Llanta sur - mercado Campesino',
+          nombre: 'Mercado Campesino',
           direccion: 'Av. panamericana',
           tel: '66-30187',
           estado: 1
         },
         {
           id: '678197a0-69a8-4c24-89a5-bf13873cc08b',
-          nombre: 'Llanta sur - mercado Abasto',
+          nombre: 'Mercado Abasto',
           direccion: 'Av. froilan tejerina',
           tel: '66-40168',
           estado: 1
